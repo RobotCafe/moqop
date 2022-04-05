83c796617821908d0e3ec2c448472951ed457a19
Milan Gladiš <hello@milangladis.com>
2022-04-05 14:03:20 +0200

    working /api/strava/:id

--- api/controller/strava.js
+++ api/controller/strava.js
@@ -48,50 +48,86 @@ exports.activity = async function(req,res) {
   const entries = await db.collection('access_tokens').get()
   // console.log(entries);
   let [{access_token, refresh_token}] = await entries.docs.map(entry => entry.data())
+  console.log('tokens: ' + access_token, refresh_token)
   const resToken = await fetch(
     `https://www.strava.com/api/v3/oauth/token?client_id=${process.env.CLIENT_ID_STRAVA}&client_secret=${process.env.CLIENT_SECRET_STRAVA}&grant_type=refresh_token&refresh_token=${refresh_token}`,
     {
       method: 'POST',
     },
   )
-  const {
-    access_token: newToken,
-    refresh_token: newRefreshToken,
-  } = await resToken.json()
-
-  const resStats = await fetch(
-    // 'https://www.strava.com/api/v3/athletes/42409445/stats',
-    // `https://www.strava.com/api/v3/activities/${event}`,
-    'https://www.strava.com/api/v3/athlete',
-    {
-      method: 'GET',
-      'Content-Type': 'application/json', 
-      headers: {
-        Authorization: `Bearer ${resToken.access_token}`,
+  .then(response => (response.json()))
+  .then(dataAuth => {
+    console.log('dataAuth')
+    console.log(dataAuth)
+    fetch(
+      // 'https://www.strava.com/api/v3/athletes/42409445/stats',
+      `https://www.strava.com/api/v3/activities/${req.params.id}`,
+      // 'https://www.strava.com/api/v3/athlete',
+      {
+        method: 'GET',
+        'Content-Type': 'application/json', 
+        headers: {
+          Authorization: `Bearer ${dataAuth.access_token}`,
+        },
       },
-    },
-  )
-  .then(
-    response => {
-      console.log(response.json())
-      return res.send(response);
-      // setActivityData(response)
-    }
-  )
-  .then(data => {
-      console.log(data)
-      return res.send(data);
-      // setActivityData(data)
+    )
+    .then(
+      response => (response.json())
+    )
+    .then(dataActivity => {
+        console.log(dataActivity)
+        const {
+          access_token: newToken,
+          refresh_token: newRefreshToken,
+        } = dataAuth
+
+        
+        // console.log('tokens: ' + access_token, refresh_token)
+        db.collection('access_tokens')
+          .doc('92x9OctuhXthN1dmsW4z')
+          .update({
+              access_token: newToken,
+              refresh_token: newRefreshToken,
+            })
+        return res.send(dataActivity);
+        // setActivityData(data)
+    })
   })
 
-  console.log('tokens: ' + access_token, refresh_token)
-  db.collection('access_tokens')
-    .doc('92x9OctuhXthN1dmsW4z')
-    .update({
-        access_token: newToken,
-        refresh_token: newRefreshToken,
-      })
+  // const resStats = fetch(
+  //   // 'https://www.strava.com/api/v3/athletes/42409445/stats',
+  //   // `https://www.strava.com/api/v3/activities/${event}`,
+  //   'https://www.strava.com/api/v3/athlete',
+  //   {
+  //     method: 'GET',
+  //     'Content-Type': 'application/json', 
+  //     headers: {
+  //       Authorization: `Bearer ${resToken.access_token}`,
+  //     },
+  //   },
+  // )
+  // .then(
+  //   response => {
+  //     // console.log(response.json())
+  //     return res.send(response);
+  //     // setActivityData(response)
+  //   }
+  // )
+  // .then(data => {
+  //     console.log(data)
+  //     return res.send(data);
+  //     // setActivityData(data)
+  // })
 
+  // console.log('tokens: ' + access_token, refresh_token)
+  // db.collection('access_tokens')
+  //   .doc('92x9OctuhXthN1dmsW4z')
+  //   .update({
+  //       access_token: newToken,
+  //       refresh_token: newRefreshToken,
+  //     })
+
+    // return res.send(resStats)      
 
   // return res.send(`Fetched activity from strava: ${req.params.id}`);
 }
