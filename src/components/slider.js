f52c91701da19b4258860e194285f5ddffb132bf
Milan Gladiš <hello@milangladis.com>
2022-10-18 23:58:00 +0200

    fixed carousel with images on the main page

--- src/components/slider.js
+++ src/components/slider.js
@@ -23,15 +23,15 @@ export default function Slider() {
   return (
     <div className="mt-48">
       <div className="relative max-w-full overflow-hidden no-scrollbar">
-        <div className="flex justify-center gap-8 md:gap-16 p-8 outline-hidden h-[400px]" style={{ transform: `translateX(-${scrollY/20}px)` }}>
-          <div className='h-[400] aspect-9/16 '><Image src="/images/examples/1.jpg" width="225" height="400" className="h-full w-auto rounded flex-1" /></div>
-          <div className='h-[400] aspect-9/16 '><Image src="/images/examples/2.jpg" width="225" height="400" className="h-full w-auto rounded flex-1" /></div>
-          <div className='h-[400] aspect-9/16 '><Image src="/images/examples/3.jpg" width="225" height="400" className="h-full w-auto rounded flex-1" /></div>
-          <div className='h-[400] aspect-9/16 hidden xs:block'><Image src="/images/examples/4.jpg" width="225" height="400" className="h-full w-auto rounded flex-1" /></div>
-          <div className='h-[400] aspect-9/16 hidden sm:block'><Image src="/images/examples/5.jpg" width="225" height="400" className="h-full w-auto rounded flex-1" /></div>
-          <div className='h-[400] aspect-9/16 hidden md:block'><Image src="/images/examples/1.jpg" width="225" height="400" className="h-full w-auto rounded flex-1" /></div>
-          <div className='h-[400] aspect-9/16 hidden lg:block'><Image src="/images/examples/2.jpg" width="225" height="400" className="h-full w-auto rounded flex-1" /></div>
-          <div className='h-[400] aspect-9/16 hidden lg:block'><Image src="/images/examples/3.jpg" width="225" height="400" className="h-full w-auto rounded flex-1" /></div>
+        <div className="flex justify-center gap-8 md:gap-16 p-8 outline-hidden w-full">
+          <div className='h-[400] '><Image src="/images/examples/1.jpg" width="225" height="400" className="rounded w-[225px] h-[400]" /></div>
+          <div className='h-[400] '><Image src="/images/examples/2.jpg" width="225" height="400" className="rounded w-[225px] h-[400]" /></div>
+          <div className='h-[400]'><Image src="/images/examples/3.jpg" width="225" height="400" className="rounded w-[225px] h-[400]" /></div>
+          <div className='h-[400] hidden sm:block'><Image src="/images/examples/4.jpg" width="225" height="400" className="rounded w-[225px] h-[400]" /></div>
+          <div className='h-[400] hidden sm:block'><Image src="/images/examples/5.jpg" width="225" height="400" className="rounded w-[225px] h-[400]" /></div>
+          <div className='h-[400] hidden md:block'><Image src="/images/examples/1.jpg" width="225" height="400" className="rounded w-[225px] h-[400]" /></div>
+          <div className='h-[400] hidden lg:block'><Image src="/images/examples/2.jpg" width="225" height="400" className="rounded w-[225px] h-[400]" /></div>
+          <div className='h-[400] hidden lg:block'><Image src="/images/examples/3.jpg" width="225" height="400" className="rounded w-[225px] h-[400]" /></div>
         </div>
       </div>
     </div>
